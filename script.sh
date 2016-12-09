627288135cccef10285a88fed7698cdb85d94d32
Mogu programming <kbu94983@gmail.com>
2016-12-09 16:13:44 +0900

    MOD: Update .scirpts.sh and .travis.yml.

---
+++ script.sh
@@ -0,0 +1,14 @@
+sudo apt-get update
+sudo apt-get -y install libopencv-dev build-essential cmake git libgtk2.0-dev pkg-config python-dev python-numpy libdc1394-22 libdc1394-22-dev libjpeg-dev libpng12-dev libtiff4-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev libtbb-dev libqt4-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils unzip
+mkdir opencv
+cd opencv
+wget https://github.com/Itseez/opencv/archive/3.1.0.zip
+unzip 3.0.0-beta.zip
+cd opencv-3.0.0-beta
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
+make
+sudo make install
+sudo /bin/bash -c 'echo &amp;quot;/usr/local/lib&amp;quot; &amp;gt; /etc/ld.so.conf.d/opencv.conf'
+sudo ldconfig
