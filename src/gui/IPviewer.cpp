c4647fb972666bdc06b43815e04b78f83f7b063c
Mogu programming <kbu94983@gmail.com>
2016-12-14 21:54:03 +0900

    MOD: in mainwindow.

--- src/gui/IPviewer.cpp
+++ src/gui/IPviewer.cpp
@@ -1,10 +1,16 @@
-#include "IPviewer.hpp"
-#include <QLabel>
-#include <QWindow>
 #include <QtWidgets>
+#include <QCommandLineParser>
+#include <QCommandLineOption>
 
+#include "IPviewer.hpp"
+#include "MainWindow.hpp"
 
-int IpApp::init(int argc,char** argv){
+int IpApp::init(int argc,char** argv)
+{
+    Q_INIT_RESOURCE(application);
+
+    //QApplication app(argc, argv);
+    
     return 1;
 }
 
