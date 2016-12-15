191824ee92f08d2795b9bbec1090d66bc0d7d9a4
Mogu programming <kbu94983@gmail.com>
2016-12-15 15:20:55 +0900

    MOD: about qt.

--- src/gui/IPviewer.cpp
+++ src/gui/IPviewer.cpp
@@ -1,4 +1,6 @@
 #include <QtWidgets>
+#include <QApplication>
+#include <QtGui>
 #include <QCommandLineParser>
 #include <QCommandLineOption>
 
@@ -9,8 +11,22 @@ int IpApp::init(int argc,char** argv)
 {
     Q_INIT_RESOURCE(application);
 
-    //QApplication app(argc, argv);
-    
-    return 1;
+    QApplication app(argc, argv);
+    QCoreApplication::setOrganizationName("QtProject");
+    QCoreApplication::setApplicationName("Application Example");
+    QCoreApplication::setApplicationVersion(QT_VERSION_STR);
+    QCommandLineParser parser;
+
+    parser.setApplicationDescription(QCoreApplication::applicationName());
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addPositionalArgument("file", "The file to open.");
+    parser.process(app);
+
+    MainWindow mainWin;
+    if (!parser.positionalArguments().isEmpty())
+        mainWin.loadFile(parser.positionalArguments().first());
+    mainWin.show();
+    return app.exec();
 }
 
--- src/ip.cpp
+++ src/ip.cpp
@@ -1,9 +1,9 @@
 #include "gui/IPviewer.hpp"
 #include <iostream>
 
-
 int main(int argc, char** argv){
 
+    
     std::cout << "argv[1]:" << argv[1] << std::endl;
     if (!argv[1]){
         throw std::invalid_argument("argv[1]: Please Image data.");
